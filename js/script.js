09ad10e9552b402896d98fca54b195c90c6ac44a
binglegnz <619683065@qq.com>
2017-09-12 16:09:55 +0800

    9.12商城

---
+++ js/script.js
@@ -0,0 +1,11 @@
+setHtmlFontSize();
+window.onresize = function(){
+	setHtmlFontSize ();
+}
+function setHtmlFontSize (){
+	if(window.innerWidth <= 750){
+		document.documentElement.style.fontSize = ((window.innerWidth / 750)*100)+'px';		
+	}else{
+		document.documentElement.style.fontSize = '100px';
+	}
+}
